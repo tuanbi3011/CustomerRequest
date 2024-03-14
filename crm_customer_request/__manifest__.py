2ce37d66730973581ee91e40fe214d1b5413b539
tuanbi3011 <113579130+tuanbi3011@users.noreply.github.com>
2024-03-14 11:52:31 +0700

    create customer_request view

--- crm_customer_request/__manifest__.py
+++ crm_customer_request/__manifest__.py
@@ -25,6 +25,7 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
+        'view/customer_request.xml',
         'views/views.xml',
         'views/templates.xml',
     ],
