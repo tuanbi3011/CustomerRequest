d79acecc93b8376d73f562702eae1e684b5ecc4a
tuanbi3011 <113579130+tuanbi3011@users.noreply.github.com>
2024-03-18 10:01:00 +0700

    Update customer_request.py

--- crm_customer_request/models/customer_request.py
+++ crm_customer_request/models/customer_request.py
@@ -6,7 +6,7 @@ class CustomerRequest(models.Model):
     _name ='crm.customer.request'
     _description = 'CRM Customer Request'
 
-    product_id = fields.Many2one('product.template', required = 1)
+    product_id = fields.Many2one('product.template', required = 1 , readonly=True)
     opportunity_id = fields.Many2one('crm.lead', required = 1)
     date = fields.Date(required = 1 , default = fields.Date.today)
     qty = fields.Float(string='Quantity', default=1)
